5fc6bc817ae1d3328fbf00e3f51ddbc7413016e8
zhengyuanhou <zhengyuanhou@vasen.com>
2024-12-12 08:32:27 +0800

    build: 更新 hips-wx-utils 版本并重构 App 组件
    - 更新 hips-wx-utils 版本至 0.1.8-beta6
    - 重构 App 组件，使用 Vant 组件库创建表单
    - 添加多种输入类型的支持，包括日期、单选、多选等
    - 优化样式和布局，移除冗余代码

--- src/App.vue
+++ src/App.vue
@@ -1,170 +1,328 @@
+<!--
+* @description 
+* @fileName demo.vue
+* @author zheng yuanhou
+* @date 2024/12/10 09:29:16
+!-->
 <template>
-  <HipsWxPage
-    title="Demo"
-    @click-left="onClickLeft"
-    @click-right="onClickRight"
-  >
-    <template #title-right></template>
-    <template #footer></template>
-    <!-- <HipsWxCard title="demo" value="10" @click="show1 = !show1">
-      <template #label>
-        <div class="flex">
-          <div>1</div>
-          <div>2</div>
-        </div>
-      </template>
-      <template #title>
-        <div class="title">111</div>
-      </template>
-      <van-tag>111</van-tag>
-    </HipsWxCard> -->
-    <!-- <HipsWxDialog title="dialog" v-model="show">
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
-      <div>4</div>
-    </HipsWxDialog>
-    <HipsWxFloatBtn v-if="show1"></HipsWxFloatBtn> -->
-    <!-- <HipsWxList
-      :loading="loading"
-      :finished="finished"
-      @load="onLoad"
-      @refresh="onRefresh"
+  <hips-wx-page title="demo" @click-left="goBack">
+    <van-form
+      ref="form"
+      input-align="right"
+      @submit="onSubmit"
+      @failed="onFailed"
     >
-      <van-cell v-for="item in list" :key="item" :title="item" />
-    </HipsWxList> -->
-    <!-- <HipsWxSingle
-      v-model="value"
-      :meaning.sync="meaning"
-      title="demo"
-      lovCode="LOV_UNIT"
-      checkTitle="unitName"
-      checkRadio="unitId"
-      @reset="onReset"
-    >
-      <template #search>
-        <van-field name="demo" label="demo" v-model="demo"></van-field>
-      </template>
-      <template #label="{ item }">
-        <div class="flex">
-          <div>{{ item.unitName }}</div>
-          <div>{{ item.unitId }}</div>
-        </div>
-      </template>
-    </HipsWxSingle> -->
-    <!-- <HipsWxUpload v-model="list" bucketName="asset-manage-app"></HipsWxUpload> -->
-    <hips-wx-multiple
-      v-model="value"
-      :meaning.sync="meaning"
-      :singleData="list"
-      checkTitle="meaning"
-      checkRadio="value"
-    ></hips-wx-multiple>
-  </HipsWxPage>
+      <div v-for="(fieldObject, index) in fields" :key="index">
+        <hips-wx-card v-if="fieldObject.type === 'card'" v-bind="fieldObject" />
+        <hips-wx-date
+          v-if="fieldObject.type === 'date'"
+          v-model="info[fieldObject.name]"
+          v-bind="fieldObject"
+          :rules="getRules(fieldObject)"
+          :placeholder="getPlaceholder(fieldObject)"
+        />
+        <hips-wx-single
+          v-if="fieldObject.type === 'single'"
+          v-model="info[fieldObject.name]"
+          v-bind="fieldObject"
+          :meaning.sync="info[getMeaning(fieldObject.name)]"
+          :rules="getRules(fieldObject)"
+          :placeholder="getPlaceholder(fieldObject)"
+        />
+        <hips-wx-multiple
+          v-if="fieldObject.type === 'multiple'"
+          v-model="info[fieldObject.name]"
+          v-bind="fieldObject"
+          :meaning.sync="info[getMeaning(fieldObject.name)]"
+          :rules="getRules(fieldObject)"
+          :placeholder="getPlaceholder(fieldObject)"
+        />
+        <hips-wx-radio
+          v-if="fieldObject.type === 'radio'"
+          v-model="info[fieldObject.name]"
+          v-bind="fieldObject"
+          input-align="right"
+          :rules="getRules(fieldObject)"
+          :placeholder="getPlaceholder(fieldObject)"
+        />
+        <van-field
+          v-else-if="fieldObject.type === 'text'"
+          v-model="info[fieldObject.name]"
+          v-bind="fieldObject"
+          :rules="getRules(fieldObject)"
+          :placeholder="getPlaceholder(fieldObject)"
+        />
+        <van-field
+          v-else-if="fieldObject.type === 'tel'"
+          v-model="info[fieldObject.name]"
+          type="tel"
+          v-bind="fieldObject"
+          :rules="getRules(fieldObject)"
+          :placeholder="getPlaceholder(fieldObject)"
+        />
+        <van-field
+          v-else-if="fieldObject.type === 'digit'"
+          v-model="info[fieldObject.name]"
+          type="digit"
+          v-bind="fieldObject"
+          :rules="getRules(fieldObject)"
+          :placeholder="getPlaceholder(fieldObject)"
+        />
+        <van-field
+          v-else-if="fieldObject.type === 'number'"
+          v-model="info[fieldObject.name]"
+          type="number"
+          v-bind="fieldObject"
+          :rules="getRules(fieldObject)"
+          :placeholder="getPlaceholder(fieldObject)"
+        />
+        <van-field
+          v-else-if="fieldObject.type === 'password'"
+          v-model="info[fieldObject.name]"
+          type="password"
+          v-bind="fieldObject"
+          :rules="getRules(fieldObject)"
+          :placeholder="getPlaceholder(fieldObject)"
+        />
+      </div>
+    </van-form>
+    <template #footer>
+      <van-button
+        type="primary"
+        size="large"
+        native-type="submit"
+        form="form"
+        :loading="loading"
+        @click="$refs.form.submit()"
+      >
+        提交
+      </van-button>
+    </template>
+  </hips-wx-page>
 </template>
 
 <script>
-// import HelloWorld from "./components/HelloWorld.vue";
-import HipsWxPage from "./components/HipsWxPage.vue";
-// import HipsWxCard from "./components/HipsWxCard.vue";
-// import HipsWxDialog from "./components/HipsWxDialog.vue";
-// import HipsWxFloatBtn from "./components/HipsWxFloatBtn.vue";
-// import HipsWxList from "./components/HipsWxList.vue";
-// import HipsWxUpload from "./components/HipsWxUpload.vue";
-// import { Field, Tag } from "vant";
-// import HipsWxSingle from "./components/HipsWxSingle.vue";
-import { bridge } from "hips-wx-utils";
-// import HipsWxDates from "./components/HipsWxDates.vue";
-import HipsWxMultiple from "./components/HipsWxMultiple.vue";
+/** ===== import ===== */
+import {
+  HipsWxPage,
+  HipsWxSingle,
+  HipsWxCard,
+  HipsWxRadio,
+  HipsWxMultiple,
+  HipsWxUpload,
+  HipsWxDate,
+} from "./components";
+import { Form, Field, Button } from "vant";
+/** ===== import ===== */
 
 export default {
-  name: "App",
+  // 组件名称
+  name: "DemoApp",
+  /** ===== components ===== */
   components: {
-    // HelloWorld
-    HipsWxPage,
-    // HipsWxCard,
-    // HipsWxDialog,
-    // HipsWxFloatBtn,
-    // HipsWxList,
-    // [Field.name]: Field,
-    // [Tag.name]: Tag,
-    // HipsWxSingle,
-    // HipsWxUpload,
-    // HipsWxDates,
-    HipsWxMultiple,
+    [HipsWxPage.name]: HipsWxPage,
+    [HipsWxSingle.name]: HipsWxSingle,
+    [HipsWxCard.name]: HipsWxCard,
+    [HipsWxRadio.name]: HipsWxRadio,
+    [HipsWxMultiple.name]: HipsWxMultiple,
+    [HipsWxUpload.name]: HipsWxUpload,
+    [HipsWxDate.name]: HipsWxDate,
+    [Form.name]: Form,
+    [Field.name]: Field,
+    [Button.name]: Button,
   },
+  /** ===== components ===== */
+  // 组件参数 接收来自父组件的数据
+  props: {},
+  // 组件状态值
   data() {
     return {
-      show1: false,
-      meaning: "",
-      value: "",
-      demo: "",
-      list: [
+      loading: false,
+      info: {
+        demo2: "",
+        demo2Meaning: "",
+      },
+      fields: [
         {
-          meaning: "1",
-          value: "1",
+          name: "demo",
+          type: "card",
+          title: "demo",
         },
         {
-          meaning: "2",
-          value: "2",
+          name: "demo1",
+          type: "date",
+          label: "demo1",
+          required: true,
         },
         {
-          meaning: "3",
-          value: "3",
+          name: "demo2",
+          type: "single",
+          label: "demo2",
+          // required: true,
+          lovCode: "QMS.WORKSHOP",
+          // searchKey: "workshopName",
+          // checkTitle: "workshopName",
+          // checkRadio: "workshopId",
+        },
+        {
+          name: "demo2Meaning",
+          bind: "demo2.workshopName",
+        },
+        {
+          name: "demo3",
+          type: "multiple",
+          label: "demo3",
+          // required: true,
+          lookupCode: "MOULD.PIPEORPIPEFITTING",
+          searchKey: "workshopName",
+        },
+        {
+          name: "demo3Meaning",
+          bind: "demo3.workshopName",
+        },
+        {
+          name: "demo4",
+          type: "radio",
+          label: "demo4",
+          required: true,
+        },
+        {
+          name: "demo5",
+          type: "text",
+          label: "demo5",
+          required: true,
+        },
+        {
+          name: "demo6",
+          type: "tel",
+          label: "demo6",
+          required: true,
+        },
+        {
+          name: "demo7",
+          type: "digit",
+          label: "demo7",
+          required: true,
+        },
+        {
+          name: "demo8",
+          type: "number",
+          label: "demo8",
+          required: true,
+        },
+        {
+          name: "demo9",
+          type: "number",
+          label: "demo9",
+          required: true,
         },
       ],
     };
   },
-  methods: {
-    onClickLeft() {
-      console.log("left");
-    },
-    onClickRight() {
-      console.log("right");
-    },
-    onRefresh() {
-      this.list = [];
-    },
-    onReset() {
-      this.demo = "";
-    },
-    onLoad() {
-      // 异步更新数据
-      // setTimeout 仅做示例，真实场景中一般为 ajax 请求
-      setTimeout(() => {
-        for (let i = 0; i < 10; i++) {
-          this.list.push(this.list.length + 1);
-        }
-        this.loading = false;
-
-        if (this.list.length >= 40) {
-          this.finished = true;
-        } else {
-          this.finished = false;
-        }
-      }, 1000);
+  // 计算属性
+  computed: {
+    binds() {
+      return this.fields.filter((item) => item.bind);
     },
   },
+  // 路由组件被激活时触发
+  activated() {
+    /** ===== activated ===== */
+    // this.init();
+    /** ===== activated ===== */
+  },
+  // 路由组件失活时触发
+  deactivated() {
+    /** ===== deactivated ===== */
+    this.unInit();
+    /** ===== deactivated ===== */
+  },
+  // 组件生成时触发
   created() {
-    bridge.getBaseInfo();
+    /** ===== created ===== */
+    this.init();
+    /** ===== created ===== */
+  },
+  // 组件生成完毕后触发
+  mounted() {
+    /** ===== mounted ===== */
+    /** ===== mounted ===== */
+  },
+  // 组件方法
+  methods: {
+    goBack() {},
+    init() {
+      setTimeout(() => {
+        this.info.demo2 = "18";
+        this.info.demo2Meaning = "管道制造部";
+        this.$forceUpdate();
+      }, 500);
+    },
+    unInit() {},
+    onSubmit() {
+      if (this.loading) {
+        return false;
+      }
+      this.loading = true;
+      console.log("🚀 ~ onSubmit ~ this.info:", this.info);
+      setTimeout(() => {
+        this.loading = false;
+      }, 3000);
+    },
+    onFailed(err) {
+      console.log("🚀 ~ onFailed ~ err:", err);
+    },
+    getMeaning(name = "") {
+      const reg = new RegExp(`^${name}.`);
+      const item = this.binds.find((item) => {
+        return reg.test(item.bind);
+      });
+      if (item) {
+        return item.name;
+      }
+      return name;
+    },
+    getRules(props) {
+      const {
+        rules = [],
+        required = false,
+        label = "",
+        title = label,
+        type = "text",
+      } = props;
+      if (rules.length > 0) {
+        return rules;
+      }
+      switch (type) {
+        case "date":
+        case "single":
+        case "multiple":
+          return [{ required, message: `请选择${title}` }];
+        default:
+          return [{ required, message: `请输入${title}` }];
+      }
+    },
+    getPlaceholder(props) {
+      const {
+        placeholder = "",
+        type = "text",
+        label = "",
+        title = label,
+      } = props;
+      if (placeholder) {
+        return placeholder;
+      }
+      switch (type) {
+        case "date":
+        case "single":
+        case "multiple":
+          return `请选择${title}`;
+        default:
+          return `请输入${title}`;
+      }
+    },
   },
 };
 </script>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-.title {
-  font-weight: bold;
-}
-.flex {
-  display: flex;
-  justify-content: space-between;
-}
-</style>
+<style lang="less" scoped></style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import App from "./App.vue";
 
 Vue.config.productionTip = false;
 
-console.log(1);
-
 new Vue({
   render: (h) => h(App),
 }).$mount("#app");
